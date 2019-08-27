f02545ae10c89f3f3aa7060726dee18d869b00fc
Brian <briantechdom@gmail.com>
2019-08-27 09:12:28 -0700

    Added node-persist

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log('starting password manager');
+
+var storage = require('node-persist');
+
+storage.initSync();
+// writes and syncs variables
+
+// storage.setItemSync('name', 'Brian');
+// saves new variable to machine
+
+var accounts = storage.getItemSync('accounts');
+
+accounts.push({
+    username: 'Joy',
+    balance: 100
+});
+
+storage.setItemSync('accounts', accounts);
+
+console.log(accounts);
